88e0c50ef3fa13e65fb11c5f760f26af9ceb7d63
Amin savari <amin.savari91@yahoo.com>
2019-10-09 14:58:42 +0330

    add Message service layer

--- CMS_NetCore.Interfaces/IMessageService.cs
+++ CMS_NetCore.Interfaces/IMessageService.cs
@@ -4,17 +4,16 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using CMS_NetCore.DomainClasses;
-using  CMS_NetCore.ViewModels;
+using CMS_NetCore.ViewModels;
 
 namespace CMS_NetCore.Interfaces
 {
     public interface IMessageService
     {
-        DataGridViewModel<Message> GetBySearch(int page, int pageSize, string searchString ,string identity);
-        Message GetById(int? id);
-        void Add(Message message);
-        void Edit(Message message);
-        void Delete(Message message);
-        void Delete(int? id);
+        Task<DataGridViewModel<Message>> GetBySearch(int page, int pageSize, string searchString ,string identity);
+        Task<Message> GetById(int? id);
+        Task Add(Message message);
+        Task Edit(Message message);
+        Task Remove(Message message);
     }
 }
---
+++ CMS_NetCore.ServiceLayer/EfMessageService.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CMS_NetCore.DomainClasses;
+using CMS_NetCore.DataLayer;
+using CMS_NetCore.ViewModels;
+using CMS_NetCore.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace Koshop.ServiceLayer
+{
+    public class EfMessageService : RepositoryBase<Message>,IMessageService
+    {
+
+        public EfMessageService(AppDbContext context) : base(context)
+        {
+        }
+
+        public async Task<DataGridViewModel<Message>> GetBySearch(int page, int pageSize, string searchString,string identity)
+        {
+            var dataGridView = new DataGridViewModel<Message>
+            {
+                Records = await FindByCondition(x => (x.UsersFrom.moblie == identity)
+                && (x.UsersFrom.moblie.Contains(searchString) || x.Subject.Contains(searchString)))
+                .OrderBy(o => o.MessageId).Include(x=>x.UsersFrom)
+                .Skip((page - 1) * pageSize).Take(pageSize).ToListAsync(),
+
+                TotalCount = await FindByCondition(x => (x.UsersFrom.moblie == identity)
+                && (x.UsersFrom.moblie.Contains(searchString) || x.Subject.Contains(searchString)))
+                .OrderBy(o => o.MessageId).Include(x=>x.UsersFrom).CountAsync(),
+            };
+
+            return dataGridView;
+        }
+        public async Task Add(Message message)
+        {
+            message.AddedDate = DateTime.Now;
+            message.ModifiedDate = DateTime.Now;
+            message.ISRead = false;
+            Create(message);
+            await SaveAsync();
+        }
+
+        public async Task Remove(Message message)
+        {
+            Delete(message);
+            await SaveAsync();
+        }
+
+
+        public async Task Edit(Message message)
+        {
+            message.ISRead = true;
+            message.ModifiedDate = DateTime.Now;
+            Update(message);
+            await SaveAsync();
+        }
+
+        public async Task<Message> GetById(int? id) =>
+            await FindByCondition(x => x.MessageId.Equals(id)).DefaultIfEmpty(new Message()).SingleAsync();
+    }
+}
